6ab73bd246ed0117c4d78cd58c66e7ace7efbad4
harsh-bairagi4 <149129549+harsh-bairagi4@users.noreply.github.com>
2025-12-26 03:13:25 +0530

    Delete TestSpringMvc directory

--- TestSpringMvc/src/main/java/com/harsh/test/repository/CourseRepository.java
+++
@@ -1,9 +0,0 @@
-package com.harsh.test.repository;
-
-import com.harsh.test.entity.Course;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CourseRepository extends JpaRepository<Course, Integer> {
-}
