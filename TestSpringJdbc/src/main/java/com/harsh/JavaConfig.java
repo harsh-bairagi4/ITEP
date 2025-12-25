d2b722b31aaa607757da154d5ea5af56ed224df8
harsh-bairagi4 <harskpw592@gmail.com>
2025-12-26 01:10:37 +0530

    spring-jdbc-practice

---
+++ TestSpringJdbc/src/main/java/com/harsh/JavaConfig.java
@@ -0,0 +1,31 @@
+package com.harsh;
+
+import com.harsh.dao.StudentDAO;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+@Configuration
+public class JavaConfig {
+    @Bean
+    public DriverManagerDataSource getDataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
+        dataSource.setUrl("jdbc:mysql://localhost:3306/testspring");
+        dataSource.setUsername("");
+        dataSource.setPassword("");
+        return dataSource;
+    }
+    @Bean
+    public JdbcTemplate getTemplate() {
+        JdbcTemplate template = new JdbcTemplate();
+        template.setDataSource(getDataSource());
+        return template;
+    }
+    @Bean
+    public StudentDAO getStudentDAO() {
+        StudentDAO dao = new StudentDAO();
+        return dao;
+    }
+}
